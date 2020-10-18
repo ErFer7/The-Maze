86d3ece7c986df293a00732dec113de87ec8c30a
Eric Fernandes Evaristo <ericfernandes7@gmail.com>
2020-10-18 17:45:53 -0300

    PlayGamesButtons.cs refatorado

--- Assets/Scripts/Menu Scripts/PlayGamesButtons.cs
+++ Assets/Scripts/Menu Scripts/PlayGamesButtons.cs
@@ -3,49 +3,27 @@ using UnityEngine.UI;
 
 public class PlayGamesButtons : MonoBehaviour
 {
+    #region Public Variables
+    // Define o estado de espera do botão
     [System.NonSerialized]
     public bool freeToProceed;
+    #endregion
 
+    #region Private Variables
+    // Objeto que permite a interação com o plugin do Play Games
     private PlayGamesManager playGamesManager;
+
+    // Evento de tecla pressionada
     private Event keyPressed;
+    #endregion
 
-    public void Interact()
-    {
-        if (!DataHolder.animating)
-        {
-            if (PlayerPrefs.GetInt("Authenticated", 0) == 0)
-            {
-                playGamesManager.LogIn();
-            }
-            else
-            {
-                playGamesManager.SignOut();
-            }
-        }
-    }
-
-    public void OkButton()
-    {
-        if (freeToProceed)
-        {
-            playGamesManager.MessageBoxControl(PlayGamesManager.MessageBoxState.OkButtonPressed);
-        }
-    }
-
-    public void ShowAchievements()
-    {
-        playGamesManager.ShowAchievements();
-    }
-
-    public void ShowLeaderboards()
-    {
-        playGamesManager.ShowLeaderboard();
-    }
-
+    #region Unity Methods
     private void Start()
     {
+        // Procura e define o objeto
         playGamesManager = GameObject.FindWithTag("PlayGamesManager").GetComponent<PlayGamesManager>();
 
+        // Faz a diferênciação de qual objeto esse script está anexado
         if (gameObject.name == "Achievements Button" && PlayerPrefs.GetInt("Authenticated", 0) == 0)
         {
             gameObject.GetComponent<Button>().interactable = false;
@@ -58,10 +36,56 @@ public class PlayGamesButtons : MonoBehaviour
 
     private void OnGUI()
     {
+        // Condições de pressionamente de botão
         keyPressed = Event.current;
         if (keyPressed.keyCode == KeyCode.Escape && freeToProceed && playGamesManager.playGamesMessageBoxTransform.anchoredPosition.y > -1F)
         {
             playGamesManager.MessageBoxControl(PlayGamesManager.MessageBoxState.OkButtonPressed);
         }
     }
+    #endregion
+
+    #region Interact
+    public void Interact()
+    {   
+        // Se não há nenhuma animação no momento
+        if (!DataHolder.animating)
+        {
+            // Faz o login caso o jogador não esteja logado, caso ele esteja logado faz o logoff
+            if (PlayerPrefs.GetInt("Authenticated", 0) == 0)
+            {
+                // Login
+                playGamesManager.LogIn();
+            }
+            else
+            {   
+                // Logoff
+                playGamesManager.SignOut();
+            }
+        }
+    }
+    #endregion
+
+    #region Buttons
+    public void OkButton()
+    {
+        // Comunica para o controle da caixa de texto que o botão de OK foi pressionado
+        if (freeToProceed)
+        {
+            playGamesManager.MessageBoxControl(PlayGamesManager.MessageBoxState.OkButtonPressed);
+        }
+    }
+
+    public void ShowAchievements()
+    {
+        // Exibe conquistas
+        playGamesManager.ShowAchievements();
+    }
+
+    public void ShowLeaderboards()
+    {
+        // Exibe placares
+        playGamesManager.ShowLeaderboard();
+    }
+    #endregion
 }
