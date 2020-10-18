edea1f7fff4e89c9f16a00b3e5a07f742575f0d2
Eric Fernandes Evaristo <ericfernandes7@gmail.com>
2020-10-18 17:51:37 -0300

    QuitGame.cs refatorado

--- Assets/Scripts/Menu Scripts/QuitGame.cs
+++ Assets/Scripts/Menu Scripts/QuitGame.cs
@@ -2,19 +2,27 @@
 
 public class QuitGame : MonoBehaviour
 {
+    #region Public Variables
+    // Objeto do painel do menu
     public GameObject menu;
-    public MusicManager musicManager;
 
+    // Objeto do Music Manager
+    public MusicManager musicManager;
+    #endregion
+
+    #region Methods
     public void Quit()
     {
+        // Sai do aplicativo
         Application.Quit();
     }
 
     public void Continue()
     {
-        // Hides the message box
-        if (DataHolder.animating == false)
+        // Esconde a caixa de texto
+        if (!DataHolder.animating)
         {
+            // Retorna o áudio ao normal
             if (musicManager.publicCoroutine_LPFF != null)
             {
                 StopCoroutine(musicManager.publicCoroutine_LPFF);
@@ -22,7 +30,9 @@ public class QuitGame : MonoBehaviour
             musicManager.gameObject.GetComponent<AudioLowPassFilter>().cutoffFrequency = 22000;
             musicManager.gameObject.GetComponent<AudioLowPassFilter>().enabled = false;
 
+            // Move a caixa de texto para baixo
             menu.GetComponent<MenuExit>().coroutine_MBA = StartCoroutine(menu.GetComponent<MenuExit>().MessageBoxAnimation(new Vector2(0, -485), new Color(1,1,1,0), 0.25F));
         }
     }
+    #endregion
 }
